49f273c2055fcb8a9c1a52ef2ba17c01d03acbb8
Matt Davison <matt45davison@gmail.com>
2019-08-08 15:44:11 -0700

    added in openweathermap into chartyscript

--- Webpage/chartscript.js
+++ Webpage/chartscript.js
@@ -1,4 +1,27 @@
 window.onload = function() {
+
+function weatherBalloon( cityID ) {
+  var key = 'c0794ec51c65c91d851ce2adead7a547';
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityID + '&APPID=' + key)  
+  .then(function(resp) { return resp.json() }) // Convert data to json
+  .then(function(w_data) {
+    drawWeather(w_data);
+    console.log(data);
+  })
+  .catch(function() {
+    // catch any errors
+  });
+}
+
+function drawWeather( d ) {
+  var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+  var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+  
+  document.getElementById('description').innerHTML = d.weather[0].description;
+  document.getElementById('temp').innerHTML = celcius + '&deg;';
+  document.getElementById('location').innerHTML = d.name;
+}
+
     $.ajax({
         // Dummy GET endpoint
         url: "https://elec391group3.free.beeceptor.com/",
